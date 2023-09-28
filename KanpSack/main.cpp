461cb4df7da2ad2e81b7e3e4ec4961f5d05326b7
George Rahul <75750164+georgerahul24@users.noreply.github.com>
2023-09-28 23:21:08 +0530

    lcs tabular and recursive

--- KanpSack/main.cpp
+++ KanpSack/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <chrono>
+#include <thread>
+#include <future>
+
 using namespace std;
 using namespace std::chrono;
 
@@ -88,6 +91,43 @@ public:
         }
     }
 
+    void calculateTopDownMultiThreaded(int i, int W, promise<int> &&p) {
+        if (i < 0) {
+            p.set_value(0);
+            return;
+        }
+        if (W < arr[i][0]) {
+
+            std::promise<int> p1;
+            auto f = p.get_future();
+            std::thread t(&KnapSack::calculateTopDownMultiThreaded, this, i - 1, W, std::move(p1));
+            t.join();
+            int i1 = f.get();
+            p.set_value(i1);
+            return;
+
+        } else {
+
+            std::promise<int> p1;
+            auto f1 = p.get_future();
+            std::thread t1(&KnapSack::calculateTopDownMultiThreaded, this, i - 1, W, std::move(p1));
+
+
+            std::promise<int> p2;
+            auto f2 = p.get_future();
+            std::thread t2(&KnapSack::calculateTopDownMultiThreaded, this, i - 1, W - arr[i][0], std::move(p1));
+
+            t1.join();
+            t2.join();
+
+            int i1 = f1.get();
+            int i2 = f2.get();
+
+            p.set_value(max(i1, arr[i][1] + i2));
+            return;
+        }
+    }
+
     int calculateBottomUp() {
         Arr table(arr.n);
         table.fillArrayWithZeros(arr.n);
@@ -104,12 +144,26 @@ int main() {
     auto start = high_resolution_clock::now();
     cout << endl << "The max that can be added is " << k.calculateTopDown(k.arr.n - 1, k.W) << endl;
     auto stop = high_resolution_clock::now();
-    cout << "The number of function calls is " << k.count<<endl;
+    cout << "The number of function calls is " << k.count << endl;
     auto duration = duration_cast<microseconds>(stop - start);
     cout << "Time taken by function: "
          << duration.count() << " microseconds" << endl;
 
+    cout << endl << endl << endl;
 
+
+    std::promise<int> p;
+    auto f = p.get_future();
+        start = high_resolution_clock::now();
+
+    k.calculateTopDownMultiThreaded(k.arr.n - 1, k.W, std::move(p));
+
+    cout << endl << "The max that can be added is " << f.get() << endl;
+    stop = high_resolution_clock::now();
+    cout << "The number of function calls is " << k.count << endl;
+    duration = duration_cast<microseconds>(stop - start);
+    cout << "Time taken by function: "
+         << duration.count() << " microseconds" << endl;
 
 
     return 0;
