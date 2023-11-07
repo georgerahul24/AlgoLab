21f187a4400745b2eab77fb9c43b0a52ea107ba7
George Rahul <75750164+georgerahul24@users.noreply.github.com>
2023-11-07 11:30:17 +0530

    Kruskal's WIth Union By Rank

--- Graph/main.cpp
+++ Graph/main.cpp
@@ -1,8 +1,6 @@
-#include "Kurskal'sWithLinkedLIst.hpp"
-#include "Prims.hpp"
+#include "Kruska'swithUnionByRank.hpp"
 int main() {
 
-    //Prims p("/Users/gr/Desktop/AlgoLab/Graph/Graph2.txt");
      Kruskal t("/Users/gr/Desktop/AlgoLab/Graph/Graph2.txt");
 
     return 0;
